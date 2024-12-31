9d9bb0fb7fc727e4e76c7287ba4475fd704b82ff
MadihaHanif123 <madihahanif8@gmail.com>
2024-12-31 23:31:08 +0500

    java

---
+++ index.js
@@ -0,0 +1,83 @@
+///1.Alerts..///
+// task 1://
+var a  =("welcome to javascript");
+alert(a);
+
+//task 2://
+var Name = prompt("What is your name?");
+alert("Hello" + " " + Name);
+
+
+/// Variables for string..///
+//  /task 1:///
+var a = prompt("first name ");
+var b = prompt(" last name");
+var c = a + b;
+alert(c);
+
+///task 2:///
+var A ="favorite hobby";
+ alert=("your favorite hobby is" + " " +A);
+
+
+///3.Variables for Numbers..///
+// task 1://
+var a =prompt("Current_year");
+var b =prompt("your Birth_Year");
+var c = a-b;
+alert(c);
+
+
+///task 2:///
+var a =prompt("length of rectangle");
+var b =prompt("width of rectangle");
+var c = a * b;
+alert("Area of rectangle is" + " " + c);
+
+ ///4.Variable Names Legal and Illegal..///
+ //task 1://
+ 
+ var a=prompt("what are legal variable names in javascript?");
+ alert(a);
+ var b=prompt("what are Illegal variable names in javascript?");
+ alert(b);
+
+
+//task 2://
+ var a =prompt("What is your age?");
+ var b =prompt("What is your friend's age?");
+ alert(" My age is " + " " + a + " and my friend's age is " + " " + b);
+
+///5.Math Operators..///
+//task 1://
+var a =prompt(" enter first number for addition");
+var b =prompt("second number");
+var c = a + b;
+alert("The sum is:"+ " " +c);
+
+//task 2://
+var a =prompt("enter first number multiply");
+var b =prompt("enter second number");
+var c = a * b;
+alert("The product is:"+ " " +c);
+
+//task 3://
+var a=prompt("enter first number percentage");
+var b=prompt("enter second number");
+var c= a % b;
+alert("The reaminder is:"+ ""+c);
+
+
+//challege 1://
+var a=prompt("date of birth year");
+var b=prompt("current-year");
+var c= a - b;
+alert("The difference is:"+" "+c);
+
+//challege 2://
+var a=prompt("first number");
+var b=prompt("second number");
+var c= a%b;
+alert("The dividend number is:"+ " "+ c);
+
+
